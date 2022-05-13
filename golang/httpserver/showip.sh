8175e9e60b94c8c8184c8d66c4b8b815a9752a3e
hammer <as7446@163.com>
2022-05-13 08:11:01 +0800

    Update showip.sh

--- golang/httpserver/showip.sh
+++ golang/httpserver/showip.sh
@@ -3,4 +3,3 @@
 echo "show container ip addr"
 Pid=$(docker inspect `docker ps |awk '{if($2=="eff4858/httpserver:1.0" && $NF=="httpserver")print $1}'` --format '{{ .State.Pid }}')
 sudo nsenter -t $Pid -n ip addr
-
